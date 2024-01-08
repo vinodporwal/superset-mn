24d056c9904626381fe67dac1f40c1c4f1e5d5c4
Shubhambmindnerves <shubhamb.mindnerves@gmail.com>
2024-01-08 17:32:10 +0530

    Superset image loader changes

--- superset/config.py
+++ superset/config.py
@@ -98,7 +98,7 @@ PACKAGE_JSON_FILE = str(files("superset") / "static/assets/package.json")
 #     "type": "image/png"
 #     "rel": "icon"
 # },
-FAVICONS = [{"href": "/static/assets/images/favicon.png"}]
+FAVICONS = [{"href": "/static/assets/images/suzlon-logo.png"}]
 
 
 def _try_json_readversion(filepath: str) -> str | None:
@@ -283,7 +283,7 @@ AUTH_RATE_LIMIT = "5 per second"
 # GLOBALS FOR APP Builder
 # ------------------------------
 # Uncomment to setup Your App name
-APP_NAME = "Superset"
+APP_NAME = "Suzlon"
 
 # Specify the App icon
 APP_ICON = "/static/assets/images/suzlon-logo.png"
